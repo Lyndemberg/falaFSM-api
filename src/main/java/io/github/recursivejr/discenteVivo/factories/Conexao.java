91f6e2b2058c5f79c7d33cd25dea6c9dcdbf5378
Lucas Duete <lm.duete@gmail.com>
2017-09-04 14:29:18 -0300

    Corrigindo erros na classe de conexão com o Banco de Dados no Heroku

--- src/main/java/io/github/recursivejr/discenteVivo/factories/Conexao.java
+++ src/main/java/io/github/recursivejr/discenteVivo/factories/Conexao.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String url = "jdbc:postgres://lxujnjxkxuivbs:3f6ea33fb42d35767312fccacc67d211070a7dbcb0b9a698bd0885e8cedbdd2f@ec2-54-163-233-201.compute-1.amazonaws.com:5432/dc556k0psngs08";
+    private static final String url = "jdbc:postgresql://ec2-54-163-233-201.compute-1.amazonaws.com:5432/dc556k0psngs08";
     private static final String usuario = "lxujnjxkxuivbs";
     private static final String senha = "3f6ea33fb42d35767312fccacc67d211070a7dbcb0b9a698bd0885e8cedbdd2f";
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
         Class.forName("org.postgresql.Driver");
-        return DriverManager.getConnection(url);
+        return DriverManager.getConnection(url, usuario, senha);
     }
 
 }
