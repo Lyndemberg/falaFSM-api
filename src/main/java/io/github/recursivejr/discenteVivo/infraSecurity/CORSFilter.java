08ef42153cfd1f212c04275d987b3aa36832842f
Lucas Duete <lm.duete@gmail.com>
2017-09-11 13:39:39 -0300

    Configurando CORS para corrigir problemas de acesso em ambiente de desenvolvimento.

--- src/main/java/io/github/recursivejr/discenteVivo/infraSecurity/CORSFilter.java
+++ src/main/java/io/github/recursivejr/discenteVivo/infraSecurity/CORSFilter.java
@@ -14,8 +14,8 @@ public class CORSFilter implements ContainerResponseFilter {
 
 		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
-		//headers.add("Access-Control-Allow-Origin", "*"); //Permite o Origin com qualquer URL
-		headers.add("Access-Control-Allow-Origin", "https://discente-vivo.herokuapp.com/");
+		headers.add("Access-Control-Allow-Origin", "*"); //Permite o Origin com qualquer URL
+		//headers.add("Access-Control-Allow-Origin", "https://discente-vivo.herokuapp.com/");
 		headers.add("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
         headers.add("Access-Control-Allow-Credentials", "true");
 		headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
