5a94442bd1faa4513aeeae5d24b334e5a24e7626
Lucas Duete <lm.duete@gmail.com>
2017-09-26 09:58:53 -0300

    Corrigindo Erro ao Codificar e Decodificar as Fotos em Base64.

--- src/main/java/io/github/recursivejr/discenteVivo/resources/FotoManagement.java
+++ src/main/java/io/github/recursivejr/discenteVivo/resources/FotoManagement.java
@@ -1,6 +1,7 @@
 package io.github.recursivejr.discenteVivo.resources;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
@@ -12,6 +13,7 @@ public class FotoManagement {
     public static String encodeFoto(File foto) throws IOException {
 
         byte[] bytes = new byte[(int)foto.length()];
+        new FileInputStream(foto).read(bytes);
 
         String fotoBase64 = new String(Base64.getEncoder().encodeToString(bytes));
         return fotoBase64;
@@ -21,7 +23,7 @@ public class FotoManagement {
 
         byte[] fotoBytes = Base64.getDecoder().decode(fotoBase64);
 
-        FileOutputStream fileOutputStream = new FileOutputStream(PATHNAME + fotoBytes.hashCode());
+        FileOutputStream fileOutputStream = new FileOutputStream(PATHNAME + fotoBase64.hashCode());
         fileOutputStream.write(fotoBytes);
 
         File foto = new File(PATHNAME + fotoBase64.hashCode());
