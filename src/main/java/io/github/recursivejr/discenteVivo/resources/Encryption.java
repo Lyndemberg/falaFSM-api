b140975f1e56ebf2263e269a41ce60286614775f
Lucas Duete <lm.duete@gmail.com>
2017-10-16 11:58:38 -0300

    Refatorando classe de Criptogria de Senhas do Sistema, substituindo MD5 por criptografia usando BCrypt, Adicionando Dependencia do BCrypt no pom.xml

--- src/main/java/io/github/recursivejr/discenteVivo/resources/Encryption.java
+++ src/main/java/io/github/recursivejr/discenteVivo/resources/Encryption.java
@@ -1,37 +1,56 @@
 package io.github.recursivejr.discenteVivo.resources;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * Esta Classe Fornece Serviços para Criptografica usando a
+ * API BCrypt.
+ * jBCrypt: http://www.mindrot.org/projects/jBCrypt/
+ * @author Lucas Duete
+ * @version 1.0
+ * @since 8.0, BCrypt 0.3m
+ */
 
 public class Encryption {
 
-    //Classe para critografar em md5
-	
-	private static MessageDigest md = null;
+	// Define os "pulos" usados na geração do hash(Intervalo Válido 10-31)
 
-	static {
-		try { 
-			md = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException ex) {
-			ex.printStackTrace(); 
-		}
-	} 
-	
-	private static char[] hexCodes(byte[] text) {
-		char[] hexOutput = new char[text.length * 2];
-		String hexString;
-		for (int i = 0; i < text.length; i++) {
-			hexString = "00" + Integer.toHexString(text[i]);
-			hexString.toUpperCase().getChars(hexString.length() - 2, hexString.length(), hexOutput, i * 2);
-		}
-		return hexOutput;
-	} 
-	
-	public static String encrypt(String pwd) {
-		if (md != null) {
-			return new String(hexCodes(md.digest(pwd.getBytes())));
-		} 
-		
-		return null; 
-		} 
+	private static final int rounds = 12;
+
+	/**
+	 * Este Método é usado Para Converter um Texto em uma Hash usando o BCrypt.
+	 * O Tamanho da Hash é sempre length=60.
+	 * O valor de Rounds é Definido Acima como static final int, num intervalo de 10 a 31.
+	 * O valor 12 é um padrão seguro.
+	 * Gera um handles de 128 bits .
+	 * Gera um handles 128-bit salt e Retorna um Hash.
+	 * @param password_plaintext String que Contém a Senha em texto simples que Será Hasheada.
+	 * @return String - Uma String com Length=60 Contendo uma Hash Criada pelo BCrypt(3).
+	 */
+
+	public static String encrypt(String password_plaintext) {
+		String salt = BCrypt.gensalt(rounds);
+		String hashed_password = BCrypt.hashpw(password_plaintext, salt);
+
+		return(hashed_password);
 	}
+
+	/**
+	 * Este Método pode ser Usado em Métodos como o Login onde Compara um Texto Simples (que será
+	 * hasheada) com uma Hash já Armazenada Pelo Sistema.
+	 * @param password_plaintext String que Contém a Senha em texto simples que Será Hasheada e Comparada.
+	 * @param stored_hash Hash Salva no Banco de Dados.
+	 * @return boolean - Retorna True se as Senhas forem Iguais, Retorna False se as Senhas foram Diferentes.
+	 */
+	public static boolean checkPassword(String password_plaintext, String stored_hash) {
+		boolean password_verified = false;
+
+		if(null == stored_hash || !stored_hash.startsWith("$2a$"))
+			throw new java.lang.IllegalArgumentException("Hash Inválida.");
+
+		password_verified = BCrypt.checkpw(password_plaintext, stored_hash);
+
+		return password_verified;
+	}
+
+}
