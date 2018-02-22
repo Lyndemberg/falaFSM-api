3fa2da8fc548819859a98c570549b30f933fe9f6
Lucas Duete <lm.duete@gmail.com>
2018-02-22 00:38:19 -0300

    Refatorando Login Controller para Utilizar nova Exception e Com Isso os Novos Métodos de Login.

--- src/main/java/io/github/recursivejr/discenteVivo/controllers/LoginController.java
+++ src/main/java/io/github/recursivejr/discenteVivo/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package io.github.recursivejr.discenteVivo.controllers;
 
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import javax.ws.rs.*;
@@ -8,6 +9,7 @@ import javax.ws.rs.core.Response;
 
 import io.github.recursivejr.discenteVivo.dao.Interface.AdministradorDaoInterface;
 import io.github.recursivejr.discenteVivo.dao.Interface.AlunoDaoInterface;
+import io.github.recursivejr.discenteVivo.exceptions.AutenticacaoException;
 import io.github.recursivejr.discenteVivo.factories.FabricaDaoPostgres;
 import io.github.recursivejr.discenteVivo.infraSecurity.TokenManagement;
 import io.github.recursivejr.discenteVivo.models.Administrador;
@@ -22,14 +24,16 @@ public class LoginController {
 	@Path("loginAluno/")
 	public Response loginAluno(@FormParam("login") String login,
 							   @FormParam("senha") String senha) {
-		
+
+		//Caso Nao Venha um dos Atributos ou Eles Venham Vazios Retorna BAD_REQUEST
+		if ((login == null || login.isEmpty())
+				|| (senha == null || senha.isEmpty()))
+			return Response.status(Response.Status.BAD_REQUEST).build();
+
 		try {
 			AlunoDaoInterface alunoDao = new FabricaDaoPostgres().criarAlunoDao();
-			System.out.println("login : " + login);
-			System.out.println("senha : " + senha);
 			
 			Aluno aluno = alunoDao.login(login, senha);
-
 			
 			String token = new TokenManagement().gerarToken(aluno.getMatricula(), 1);
 
@@ -38,11 +42,18 @@ public class LoginController {
 			System.gc();
 			return Response.ok(aluno).build();
 
-		} catch(Exception ex) {
+		} catch(AutenticacaoException ex) {
+			System.out.println("login : " + login);
+			System.out.println("senha : " + senha);
 			Logger.getLogger(ex.getMessage());
 			ex.printStackTrace();
 			System.gc();
 			return Response.status(Response.Status.UNAUTHORIZED).build();
+		} catch (Exception ex) {
+			Logger.getLogger(ex.getMessage());
+			ex.printStackTrace();
+			System.gc();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 		}
 	}
 
@@ -52,6 +63,11 @@ public class LoginController {
 	@Path("loginAdmin/")
 	public Response loginAdmin(@FormParam("login") String login,
 							   @FormParam("senha") String senha) {
+
+		//Caso Nao Venha um dos Atributos ou Eles Venham Vazios Retorna BAD_REQUEST
+		if ((login == null || login.isEmpty())
+				|| (senha == null || senha.isEmpty()))
+			return Response.status(Response.Status.BAD_REQUEST).build();
 		
 		try {
 			AdministradorDaoInterface adminDao = new FabricaDaoPostgres().criarAdministradorDao();
@@ -65,11 +81,19 @@ public class LoginController {
 			System.gc();
 			return Response.ok(admin).build();
 
-		} catch(Exception ex) {
+		} catch(AutenticacaoException ex) {
+			System.out.println("login : " + login);
+			System.out.println("senha : " + senha);
 			Logger.getLogger(ex.getMessage());
+			ex.printStackTrace();
 			System.gc();
 			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}	
+		} catch (Exception ex) {
+			Logger.getLogger(ex.getMessage());
+			ex.printStackTrace();
+			System.gc();
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 }
