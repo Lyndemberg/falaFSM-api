482bb9c5f288790848e7d50c4e28fbe5fd223a67
Lucas Duete <lm.duete@gmail.com>
2018-03-06 15:41:59 -0300

    Implementando Método de Geração de Relatórios de Formularios em RelatorioController

--- src/main/java/io/github/recursivejr/discenteVivo/controllers/RelatorioController.java
+++ src/main/java/io/github/recursivejr/discenteVivo/controllers/RelatorioController.java
@@ -17,9 +17,11 @@ public class RelatorioController {
     @Security(NivelAcesso.NIVEL_1)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("gerarRelatorios/enquete/{idEnquete}")
-    //Variavel param pode ser a Id da Enquete ou o Nome da Enquete
-    public Response gerarRelatorios(@PathParam("idEnquete") int idEnquete,
+    public Response gerarRelatoriosEnquete(@PathParam("idEnquete") int idEnquete,
                                     @Context Request request) {
+
+        if (idEnquete <= 0)
+            return Response.status(Response.Status.BAD_REQUEST).build();
 
         //Cria um Relatorio contando nada
         Relatorio relatorio = null;
@@ -30,6 +32,48 @@ public class RelatorioController {
 
             //tenta gerar os relatorios com base no ID da idEnquete
             relatorio = relatorioDao.gerarRelatorioEnquete(idEnquete);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.gc();
+            Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        System.gc();
+        //Sempre retorna relatorios sendo ele null ou preenchido
+
+        EntityTag etag = new EntityTag(Integer.toString(relatorio.hashCode()));
+        Response.ResponseBuilder builder = request.evaluatePreconditions(etag);
+
+        if (builder == null) {
+            builder.status(Response.Status.OK);
+            builder.tag(etag);
+        }
+
+        builder.cacheControl(CacheController.getCacheControl());
+        return builder.build();
+    }
+
+    @GET
+    @Security(NivelAcesso.NIVEL_1)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("gerarRelatorios/formulario/{idFormulario}")
+    public Response gerarRelatoriosFormulario(@PathParam("idFormulario") int idFormulario,
+                                    @Context Request request) {
+
+
+        if (idFormulario <= 0)
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+        //Cria um Relatorio contando nada
+        Relatorio relatorio = null;
+
+        try {
+            //Intancia um relatorioDaoPostgres usando a fabrica acessando somente os metodos definidos na Interface
+            RelatorioDaoInterface relatorioDao = Fabrica.criarFabricaDaoPostgres().criarRelatorioDao();
+
+            //tenta gerar os relatorios com base no ID da idEnquete
+            relatorio = relatorioDao.gerarRelatorioFormulario(idFormulario);
 
         } catch (Exception ex) {
             ex.printStackTrace();
