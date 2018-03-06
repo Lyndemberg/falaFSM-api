493644bc3abb0d10efacbfe607596f62be6e3345
Lucas Duete <lm.duete@gmail.com>
2018-03-06 15:38:43 -0300

    Implementendo Dao de Relatorio em Postgres Para Formularios.

--- src/main/java/io/github/recursivejr/discenteVivo/dao/postgres/RelatorioDaoPostgres.java
+++ src/main/java/io/github/recursivejr/discenteVivo/dao/postgres/RelatorioDaoPostgres.java
@@ -56,7 +56,38 @@ public class RelatorioDaoPostgres extends ElementoDao implements RelatorioDaoInt
 
 	@Override
 	public Relatorio gerarRelatorioFormulario(int idFormulario) {
-    	//TODO
-		return null;
+
+		String sql = String.format("SELECT Form.IdFormulario AS IdFormulario, RC.Resposta AS Resposta, " +
+				"count(*) as QuantidadeVotos FROM Formulario AS Form JOIN Campo AS Camp ON " +
+				"Form.idformulario = Camp.idformulario LEFT JOIN RespondeCampo AS RC ON Camp.idcampo = RC.idcampo " +
+				"WHERE Form.idFormulario = '%d' GROUP BY Form.IdFormulario, RC.Resposta;", idFormulario);
+
+		Relatorio relatorio = null;
+
+		List<String> opcoes = new ArrayList<>();
+		List<Integer> votos = new ArrayList<>();
+		List<Comentario> comentarios = new ArrayList<>();
+
+		try {
+			Statement stmt = getConexao().createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				opcoes.add(rs.getString("Resposta"));
+				votos.add(rs.getInt("QuantidadeVotos"));
+				comentarios = new ComentarioFormularioDaoPostgres().listarPorChave(rs.getInt("idFormulario"));
+			}
+
+			relatorio = new Relatorio(
+					opcoes,
+					votos,
+					comentarios
+			);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			Logger.getLogger(ex.getMessage());
+		}
+		return relatorio;
 	}
 }
