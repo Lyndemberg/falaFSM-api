9552d9b76b5fcc549a0e0a686c08172a1683f26a
Lucas Duete <lm.duete@gmail.com>
2018-03-06 12:34:16 -0300

    Corrigindo Utilização do Dao em RelatorioController, Adicionando Cache ao RelatorioController.

--- src/main/java/io/github/recursivejr/discenteVivo/controllers/RelatorioController.java
+++ src/main/java/io/github/recursivejr/discenteVivo/controllers/RelatorioController.java
@@ -1,11 +1,11 @@
 package io.github.recursivejr.discenteVivo.controllers;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 
 import io.github.recursivejr.discenteVivo.dao.Interface.RelatorioDaoInterface;
-import io.github.recursivejr.discenteVivo.factories.FabricaDaoPostgres;
+import io.github.recursivejr.discenteVivo.factories.Fabrica;
+import io.github.recursivejr.discenteVivo.infraSecurity.CacheController;
 import io.github.recursivejr.discenteVivo.infraSecurity.Security;
 import io.github.recursivejr.discenteVivo.infraSecurity.model.NivelAcesso;
 import io.github.recursivejr.discenteVivo.models.Relatorio;
@@ -18,14 +18,15 @@ public class RelatorioController {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("gerarRelatorios/enquete/{param}")
     //Variavel param pode ser a Id da Enquete ou o Nome da Enquete
-    public Response gerarRelatorios(@PathParam("param") String param) {
+    public Response gerarRelatorios(@PathParam("param") String param,
+                                    @Context Request request) {
 
         //Cria um Relatorio contando nada
         Relatorio relatorio = null;
 
         try {
             //Intancia um relatorioDaoPostgres usando a fabrica acessando somente os metodos definidos na Interface
-            RelatorioDaoInterface relatorioDao = new FabricaDaoPostgres().criarRelatorioDao();
+            RelatorioDaoInterface relatorioDao = Fabrica.criarFabricaDaoPostgres().criarRelatorioDao();
 
            try {
                //tenta gerar os relatorios com base no ID, se ao converter o param de String para Integer retornar erro
@@ -44,7 +45,17 @@ public class RelatorioController {
 
         System.gc();
         //Sempre retorna relatorios sendo ele null ou preenchido
-        return Response.ok(relatorio).build();
+
+        EntityTag etag = new EntityTag(Integer.toString(relatorio.hashCode()));
+        Response.ResponseBuilder builder = request.evaluatePreconditions(etag);
+
+        if (builder == null) {
+            builder.status(Response.Status.OK);
+            builder.tag(etag);
+        }
+
+        builder.cacheControl(CacheController.getCacheControl());
+        return builder.build();
     }
 
 }
